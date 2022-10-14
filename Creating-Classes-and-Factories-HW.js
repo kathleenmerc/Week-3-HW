6524f9e1af4256ec8f826402b62831778c737478
Kathleen Mercado <kathleenmerc@gmail.com>
2022-10-13 23:49:58 -0400

    Dinner is served

--- Creating-Classes-and-Factories-HW.js
+++ Creating-Classes-and-Factories-HW.js
@@ -13,27 +13,27 @@ eatFood() - log "nibble nibble"
 getPrice() - return the price
 */
 
-class Hamster {
-    constructor (owner, name, price = 15) {
-        this.owner = owner,
-        this.name = name,
-        this.price = price
-    }
+// class Hamster {
+//     constructor (owner, name, price = 15) {
+//         this.owner = owner,
+//         this.name = name,
+//         this.price = price
+//     }
 
-    wheelRun() {
-        console.log("squeak squeak")
-    }
+//     wheelRun() {
+//         console.log("squeak squeak")
+//     }
 
-    eatFood() {
-        console.log("nibble nibble")
-    }
+//     eatFood() {
+//         console.log("nibble nibble")
+//     }
 
-    getPrice() {
-        return this.price
-    }
-}
+//     getPrice() {
+//         return this.price
+//     }
+// }
 
-console.log(Hamster)
+// console.log(Hamster)
 
 
 
@@ -60,75 +60,75 @@ ageUp() - increment age, increment height, increment weight, decrement mood, inc
 buyHamster(hamster) - push the hamster object onto the hamster array, increment mood by 10, decrement bankAccount by the value of the hamster (hint: use getPrice())
 */
 
-class Person {
-    constructor (name, age, height, weight, mood, hamsters, bankAccount) {
-        this.name = name,
-        this.age = age || 0,
-        this.height = height || 0,
-        this.weight = weight || 0,
-        this.mood = mood || 0,
-        this.hamsters = [],
-        this.bankAccount = bankAccount || 0
-    }
+// class Person {
+//     constructor (name, age, height, weight, mood, hamsters, bankAccount) {
+//         this.name = name,
+//         this.age = age || 0,
+//         this.height = height || 0,
+//         this.weight = weight || 0,
+//         this.mood = mood || 0,
+//         this.hamsters = [],
+//         this.bankAccount = bankAccount || 0
+//     }
 
-    getName() {
-        return this.name
-    }
+//     getName() {
+//         return this.name
+//     }
 
-    getAge() {
-        return this.age
-    }
+//     getAge() {
+//         return this.age
+//     }
 
-    getWeight() {
-        return this.weight
-    }
+//     getWeight() {
+//         return this.weight
+//     }
 
-    greet() {
-        console.log(`Hello ${this.name}!`)
-    }
+//     greet() {
+//         console.log(`Hello ${this.name}!`)
+//     }
 
-    eat(num) {
-        if (num) {
-            this.weight = this.weight + num
-            this.mood = this.mood + num
-        } else {
-            this.weight++
-            this.mood++
-        }
-    }
+//     eat(num) {
+//         if (num) {
+//             this.weight = this.weight + num
+//             this.mood = this.mood + num
+//         } else {
+//             this.weight++
+//             this.mood++
+//         }
+//     }
 
-    exercise(num) {
-        if (num) {
-            this.weight = this.weight - num
-        } else {
-            this.weight--
-        }
-    }
+//     exercise(num) {
+//         if (num) {
+//             this.weight = this.weight - num
+//         } else {
+//             this.weight--
+//         }
+//     }
 
-    ageUp(num) {
-        if (num) {
-            this.age = this.age + num
-            this.height = this.height + num
-            this.weight = this.weight + num
-            this.mood = this.mood - num
-            this.bankAccount = this.bankAccount + (num * 10)
-        } else {
-            this.age++
-            this.height++
-            this.weight++
-            this.mood--
-            this.bankAccount += 10
-        }
-    }
+//     ageUp(num) {
+//         if (num) {
+//             this.age = this.age + num
+//             this.height = this.height + num
+//             this.weight = this.weight + num
+//             this.mood = this.mood - num
+//             this.bankAccount = this.bankAccount + (num * 10)
+//         } else {
+//             this.age++
+//             this.height++
+//             this.weight++
+//             this.mood--
+//             this.bankAccount += 10
+//         }
+//     }
 
-    buyHamster(hamsterPet) {
-        this.hamsters.push(hamsterPet)
-        this.mood += 10
-        this.bankAccount = this.bankAccount - hamsterPet.getPrice()
-    }
-}
-let hamsterPet = Hamster
-console.log(Person)
+//     buyHamster(hamsterPet) {
+//         this.hamsters.push(hamsterPet)
+//         this.mood += 10
+//         this.bankAccount = this.bankAccount - hamsterPet.getPrice()
+//     }
+// }
+// let hamsterPet = Hamster
+// console.log(Person)
 
 
 
@@ -149,38 +149,71 @@ Feel free to update or add methods to automate some of these tasks.
 11. Have Timmy exercise twice
 */
 
-//1. and 2.
-const timmy = new Person("Timmy", 5)
-console.log(timmy)
+// //1. and 2.
+// const timmy = new Person("Timmy", 5)
+// console.log(timmy)
 
-//3.
-timmy.eat(5)
-console.log(timmy)
+// //3.
+// timmy.eat(5)
+// console.log(timmy)
 
-//4.
-timmy.exercise(5)
-console.log(timmy)
+// //4.
+// timmy.exercise(5)
+// console.log(timmy)
 
-//5. 
-timmy.ageUp(9)
-console.log(timmy)
+// //5. 
+// timmy.ageUp(9)
+// console.log(timmy)
 
-//6. and 7.
-let gus = new Hamster("Timmy", "Gus")
-console.log(gus)
+// //6. and 7.
+// let gus = new Hamster("Timmy", "Gus")
+// console.log(gus)
 
-//8.
-timmy.buyHamster(gus)
-console.log(timmy)
+// //8.
+// timmy.buyHamster(gus)
+// console.log(timmy)
 
-//9.
-timmy.ageUp(15)
-console.log(timmy)
+// //9.
+// timmy.ageUp(15)
+// console.log(timmy)
 
-//10.
-timmy.eat(2)
-console.log(timmy)
+// //10.
+// timmy.eat(2)
+// console.log(timmy)
 
-//11.
-timmy.exercise(2)
-console.log(timmy)
+// //11.
+// timmy.exercise(2)
+// console.log(timmy)
+
+
+
+/*
+Chef Make Dinners
+
+Chef should be a factory of Dinner
+Add a constructor to dinner that sets the string properties, appetizer, entree and dessert.
+Add a method on chef that takes three arguments and returns a new Dinner based on those arguments.
+Have the Chef create 3 dinners, log the dinners
+*/
+
+class Dinner {
+    constructor(appetizer, entree, dessert) {
+        this.appetizer = appetizer,
+        this.entree = entree,
+        this.dessert = dessert
+    }
+}
+
+class Chef {
+    cooksNewDinner(newApp,newEntree,newDessert) {
+        let nextDinner = new Dinner(newApp,newEntree,newDessert)
+        console.log(nextDinner)
+    }
+}
+
+const headChef = new Chef
+
+headChef.cooksNewDinner("Caviar","Filet Mignon","Crème brûlée")
+headChef.cooksNewDinner("Pita and Hummus","Spinach gnocchi", "Strawberry Shortcake")
+headChef.cooksNewDinner("Buffalo Chicken Wings", "Bacon Cheeseburger", "Apple Pie")
+
